c64d0842cd508f42f308d600480ab51a625b41a4
Omid <oersha3@wgu.edu>
2023-09-16 21:54:40 -0700

    minor issue with part F, fix CheckoutServiceImpl

---
+++ src/main/java/com/example/demo/services/CheckoutServiceImpl.java
@@ -0,0 +1,62 @@
+package com.example.demo.services;
+
+import com.example.demo.dao.CustomerRepository;
+import com.example.demo.dto.Purchase;
+import com.example.demo.dto.PurchaseResponse;
+import com.example.demo.entities.Cart;
+import com.example.demo.entities.CartItem;
+import com.example.demo.entities.Customer;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository){
+        this.customerRepository=customerRepository;
+    }
+
+    @Override
+    @Transactional
+    public PurchaseResponse placeOrder(Purchase purchase) {
+
+        // retrieve the order info from dto
+        Cart cart = purchase.getCart();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        cart.setOrderTrackingNumber(orderTrackingNumber);
+
+
+        // populate cart with cartitems
+        Set<CartItem> cartItems=purchase.getCartItems();
+        cartItems.forEach(item->cart.getCartItems().add(item));
+
+        System.out.println(cart);
+
+
+        // populate customer with order
+        Customer customer=purchase.getCustomer();
+        customer.getCarts().add(cart);
+
+
+        // save to database
+        customerRepository.save(customer);
+
+        // return a response
+        return new PurchaseResponse(orderTrackingNumber);
+
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number
+        return UUID.randomUUID().toString();
+
+    }
+}
